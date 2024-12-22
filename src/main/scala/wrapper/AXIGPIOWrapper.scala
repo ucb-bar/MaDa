63bc04bf749cde4b62248f0250c0fc5d9a92369f
-T.K.- <t_k_233@outlook.com>
2024-12-21 23:08:51 -0800

    ADD: improve IP generation flow

--- src/main/scala/wrapper/AXIGPIOWrapper.scala
+++ src/main/scala/wrapper/AXIGPIOWrapper.scala
@@ -1,6 +1,8 @@
 import chisel3.{BlackBox, _}
 import chisel3.util._
 
+import java.io.PrintWriter
+
 
 class axi_gpio_0 extends BlackBox {
   val io = IO(new Bundle {
@@ -9,4 +11,31 @@ class axi_gpio_0 extends BlackBox {
     val gpio_io_o = Output(UInt(32.W))
     val gpio_io_t = Output(UInt(32.W))
   })
+
+  def generate_tcl_script(): Unit = {
+    val vivado_project_dir = "out/VivadoProject"
+    val ip_name = "AXIStreamDataFifo"
+    val ip_name_lower = ip_name.toLowerCase()
+
+    val tcl_script = new PrintWriter(s"${vivado_project_dir}/scripts/create_ip_${ip_name_lower}.tcl")
+    
+    tcl_script.println(s"create_ip -name axis_data_fifo -vendor xilinx.com -library ip -version 2.0 -module_name ${ip_name}")
+
+    tcl_script.println(s"""
+set_property -dict [list \\
+  CONFIG.HAS_TLAST {1} \\
+  CONFIG.TUSER_WIDTH {1} \\
+] [get_ips ${ip_name}]
+""")
+
+    tcl_script.println(s"generate_target {instantiation_template} [get_ips ${ip_name}]")
+    tcl_script.println("update_compile_order -fileset sources_1")
+    tcl_script.println(s"generate_target all [get_ips ${ip_name}]")
+    tcl_script.println(s"catch { config_ip_cache -export [get_ips -all ${ip_name}] }")
+    tcl_script.println(s"export_ip_user_files -of_objects [get_ips ${ip_name}] -no_script -sync -force -quiet")
+    tcl_script.println(s"create_ip_run [get_ips ${ip_name}]")
+
+    tcl_script.close()
+  }
+  generate_tcl_script()
 }
--- src/main/scala/wrapper/AXIStreamDataFIFO.scala
+++ src/main/scala/wrapper/AXIStreamDataFIFO.scala
@@ -1,8 +1,10 @@
 import chisel3.{BlackBox, _}
 import chisel3.util._
 
+import java.io.PrintWriter
 
-class axis_data_fifo_0(width: Int) extends BlackBox {
+
+class AXIStreamDataFifo(width: Int) extends BlackBox {
   val io = IO(new Bundle {
     val s_axis_aresetn = Input(Reset())
     val s_axis_aclk = Input(Clock())
@@ -17,4 +19,31 @@ class axis_data_fifo_0(width: Int) extends BlackBox {
     val m_axis_tlast = Output(Bool())
     val m_axis_tuser = Output(Bool())
   })
+
+  def generate_tcl_script(): Unit = {
+    val vivado_project_dir = "out/VivadoProject"
+    val ip_name = "AXIStreamDataFifo"
+    val ip_name_lower = ip_name.toLowerCase()
+
+    val tcl_script = new PrintWriter(s"${vivado_project_dir}/scripts/create_ip_${ip_name_lower}.tcl")
+    
+    tcl_script.println(s"create_ip -name axis_data_fifo -vendor xilinx.com -library ip -version 2.0 -module_name ${ip_name}")
+
+    tcl_script.println(s"""
+set_property -dict [list \\
+  CONFIG.HAS_TLAST {1} \\
+  CONFIG.TUSER_WIDTH {1} \\
+] [get_ips ${ip_name}]
+""")
+
+    tcl_script.println(s"generate_target {instantiation_template} [get_ips ${ip_name}]")
+    tcl_script.println("update_compile_order -fileset sources_1")
+    tcl_script.println(s"generate_target all [get_ips ${ip_name}]")
+    tcl_script.println(s"catch { config_ip_cache -export [get_ips -all ${ip_name}] }")
+    tcl_script.println(s"export_ip_user_files -of_objects [get_ips ${ip_name}] -no_script -sync -force -quiet")
+    tcl_script.println(s"create_ip_run [get_ips ${ip_name}]")
+
+    tcl_script.close()
+  }
+  generate_tcl_script()
 }
