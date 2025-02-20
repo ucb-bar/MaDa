820d45f08e8c1cd0b3952179fadb4153cf196834
Lux <iansseijelly@berkeley.edu>
2025-02-20 11:20:35 -0800

    FIX: naming

--- ChiselProject/src/ip/xilinx/ClockingWizard.scala
+++ ChiselProject/src/ip/xilinx/ClockingWizard.scala
@@ -3,17 +3,48 @@ import chisel3.util._
 
 import java.io.PrintWriter
 
-
 class ClockingWizard(
   clk_freqs: Seq[Int]
-) extends BlackBox {
-  require(clk_freqs.length > 0, "clk_freqs must be non-empty")
-  require(clk_freqs.length <= 8, "clk_freqs must be at most 8")
+) extends RawModule {
   val io = IO(new Bundle {
     val clk_in = Input(Clock())
     val reset = Input(Bool())
     val locked = Output(Bool())
     val clk_outs = Output(Vec(clk_freqs.length, Clock()))
+  })
+  require(clk_freqs.length > 0, "clk_freqs must be non-empty")
+  require(clk_freqs.length <= 7, "clk_freqs must be at most 7")
+  val blackbox = Module(new ClockingWizardBlackbox(clk_freqs))
+  blackbox.io.clk_in1 := io.clk_in
+  blackbox.io.reset := io.reset
+  io.locked := blackbox.io.locked
+  io.clk_outs(0) := blackbox.io.clk_out1
+  if (clk_freqs.length > 1) { io.clk_outs(1) := blackbox.io.clk_out2 }
+  if (clk_freqs.length > 2) { io.clk_outs(2) := blackbox.io.clk_out3 }
+  if (clk_freqs.length > 3) { io.clk_outs(3) := blackbox.io.clk_out4 }
+  if (clk_freqs.length > 4) { io.clk_outs(4) := blackbox.io.clk_out5 }
+  if (clk_freqs.length > 5) { io.clk_outs(5) := blackbox.io.clk_out6 }
+  if (clk_freqs.length > 6) { io.clk_outs(6) := blackbox.io.clk_out7 }
+}
+
+class ClockingWizardBlackbox(
+  clk_freqs: Seq[Int]
+) extends BlackBox {
+
+    def get_used(i: Int) = i < clk_freqs.length
+    def get_freq(i: Int) = if (get_used(i)) clk_freqs(i) else 0
+
+  val io = IO(new Bundle {
+    val clk_in1 = Input(Clock())
+    val reset = Input(Bool())
+    val locked = Output(Bool())
+    val clk_out1 = Output(Clock())
+    val clk_out2 = if (get_used(1)) Output(Clock()) else null
+    val clk_out3 = if (get_used(2)) Output(Clock()) else null
+    val clk_out4 = if (get_used(3)) Output(Clock()) else null
+    val clk_out5 = if (get_used(4)) Output(Clock()) else null
+    val clk_out6 = if (get_used(5)) Output(Clock()) else null
+    val clk_out7 = if (get_used(6)) Output(Clock()) else null
   })
 
   def generate_tcl_script(): Unit = {
@@ -22,8 +53,6 @@ class ClockingWizard(
     val ip_name_lower = ip_name.toLowerCase()
 
     val num_out_clks = clk_freqs.length
-    def get_used(i: Int) = i < clk_freqs.length
-    def get_freq(i: Int) = if (get_used(i)) clk_freqs(i) else 0
     
     val tcl_script = new PrintWriter(s"${vivado_project_dir}/scripts/create_ip_${ip_name_lower}.tcl")
     
