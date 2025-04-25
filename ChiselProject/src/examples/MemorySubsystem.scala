623bfc058719ed9ded0da155e2d4c69f67ff8bf4
-T.K.- <t_k_233@outlook.com>
2025-04-25 09:46:40 -0700

    ADD: stage mem change, still bugging on vivado

--- ChiselProject/src/examples/MemorySubsystem.scala
+++ ChiselProject/src/examples/MemorySubsystem.scala
@@ -7,12 +7,12 @@ class MemorySubsystem extends Module {
     val m_axi = Flipped(new Axi4Bundle())
   })
 
-  // val xbar = Module(new Axi4Crossbar(
-  //   numSlave = 1,
-  //   numMaster = 2,
-  //   deviceSizes = Array(0x0001_0000, 0x0001_0000),
-  //   deviceAddresses = Array(0x0000_0000, 0x0001_0000),
-  // ))
+  val xbar = Module(new Axi4Crossbar(
+    numSlave = 1,
+    numMaster = 2,
+    deviceSizes = Array(0x0001_0000, 0x0001_0000),
+    deviceAddresses = Array(0x0000_0000, 0x0001_0000),
+  ))
   
 
   // val mem = Module(new Axi4BlockMemory(
@@ -31,7 +31,7 @@ class MemorySubsystem extends Module {
     )
   ))
 
-  mem.io.s_axi <> io.m_axi
+  // mem.io.s_axi <> io.m_axi
   
   // val mem = Module(new Axi4LiteMemory(
   //   params=Axi4Params(
@@ -41,17 +41,17 @@ class MemorySubsystem extends Module {
   //   )
   // ))
 
-  // val flash = Module(new Axi4QuadSpi())
+  val flash = Module(new Axi4QuadSpi())
   
-  // xbar.io.s_axi(0) <> io.m_axi
+  xbar.io.s_axi(0) <> io.m_axi
   // mem.io.s_axi.connectFromAxi4(xbar.io.m_axi(0))
-  // mem.io.s_axi <> xbar.io.m_axi(0)
-  // flash.io.s_axi4 <> xbar.io.m_axi(1)
+  mem.io.s_axi <> xbar.io.m_axi(0)
+  flash.io.s_axi4 <> xbar.io.m_axi(1)
 
-  // flash.io.ext_spi_clk := clock
-  // flash.io.s_axi := DontCare
-  // flash.io.io0_i := false.B
-  // flash.io.io1_i := false.B
-  // flash.io.sck_i := false.B
-  // flash.io.ss_i := false.B
+  flash.io.ext_spi_clk := clock
+  flash.io.s_axi := DontCare
+  flash.io.io0_i := false.B
+  flash.io.io1_i := false.B
+  flash.io.sck_i := false.B
+  flash.io.ss_i := false.B
 }
--- ChiselProject/src/generators/bili/memory/Axi4Memory.scala
+++ ChiselProject/src/generators/bili/memory/Axi4Memory.scala
@@ -24,21 +24,20 @@ class Axi4Memory(
     memoryFileBin = memoryFileBin
   ))
   
-  val reg_aw_pending = RegInit(false.B)
+  val reg_aw_active = RegInit(false.B)
   val reg_aw_id = RegInit(0.U(params.idWidth.W))
   val reg_aw_addr = RegInit(0.U(memAddressWidth.W))
   
-  val reg_w_pending = RegInit(false.B)
+  val reg_w_active = RegInit(false.B)
   val reg_w_data = RegInit(0.U(params.dataWidth.W))
   val reg_w_strb = RegInit(0.U((params.dataWidth/8).W))
 
-  val reg_ar_pending = RegInit(false.B)
-  val reg_r_data_received = RegInit(false.B)
+  val reg_ar_active = RegInit(false.B)
+  val reg_r_updating = RegInit(false.B)
   val reg_ar_id = RegInit(0.U(params.idWidth.W))
   val reg_r_data = RegInit(0.U(params.dataWidth.W))
 
-  // val write_valid = (reg_aw_pending || io.s_axi.aw.fire) && (reg_w_pending || io.s_axi.w.fire)
-  val write_valid = reg_aw_pending && reg_w_pending
+  val write_valid = reg_aw_active && reg_w_active
 
   mem.io.clock := clock
   mem.io.reset := reset
@@ -48,51 +47,61 @@ class Axi4Memory(
   mem.io.waddr := reg_aw_addr
   mem.io.wdata := reg_w_data
   mem.io.wstrb := Mux(write_valid, reg_w_strb, 0.U((params.dataWidth/8).W))
-  io.s_axi.r.bits.data := Mux(reg_r_data_received, mem.io.rdata, reg_r_data)
+  io.s_axi.r.bits.data := Mux(reg_r_updating, mem.io.rdata, reg_r_data)
 
   // control line connections
-  io.s_axi.aw.ready := !reg_aw_pending
-  io.s_axi.w.ready := !reg_w_pending
-  io.s_axi.b.valid := reg_aw_pending && reg_w_pending
+  io.s_axi.aw.ready := ~reg_aw_active
+  io.s_axi.w.ready := ~reg_w_active
+  io.s_axi.b.valid := write_valid
   io.s_axi.b.bits.id := reg_aw_id
   io.s_axi.b.bits.resp := AxResponse.OKAY
 
   when (io.s_axi.aw.fire) {
-    reg_aw_pending := true.B
+    reg_aw_active := true.B
     reg_aw_addr := io.s_axi.aw.bits.addr(memAddressWidth-1, memAlignment)
     reg_aw_id := io.s_axi.aw.bits.id
   }
 
   when (io.s_axi.w.fire) {
-    reg_w_pending := true.B
+    reg_w_active := true.B
     reg_w_data := io.s_axi.w.bits.data
     reg_w_strb := io.s_axi.w.bits.strb
   }
 
   when (io.s_axi.b.fire) {
-    reg_aw_pending := false.B
-    reg_w_pending := false.B
+    reg_aw_active := false.B
+    reg_w_active := false.B
   }
 
+  // get around Vivado sim bug
+  dontTouch(io.s_axi.aw.fire)
+  dontTouch(io.s_axi.w.fire)
+  dontTouch(io.s_axi.b.fire)
+  dontTouch(write_valid)
+
   io.s_axi.ar.ready := true.B
-  io.s_axi.r.valid := reg_ar_pending
+  io.s_axi.r.valid := reg_ar_active
   io.s_axi.r.bits.id := reg_ar_id
   io.s_axi.r.bits.resp := AxResponse.OKAY
   io.s_axi.r.bits.last := true.B
 
   when (io.s_axi.ar.fire) {
-    reg_ar_pending := true.B
+    reg_ar_active := true.B
     reg_ar_id := io.s_axi.ar.bits.id
-    reg_r_data_received := true.B
+    reg_r_updating := true.B
   }
 
   when (io.s_axi.r.fire) {
-    reg_ar_pending := false.B
+    reg_ar_active := false.B
   }
 
-  when (reg_r_data_received) {
+  // get around Vivado sim bug
+  dontTouch(io.s_axi.ar.fire)
+  dontTouch(io.s_axi.r.fire)
+
+  when (reg_r_updating) {
     reg_r_data := mem.io.rdata
-    reg_r_data_received := false.B
+    reg_r_updating := false.B
   }
   
   def generate_tcl_script(): Unit = {
